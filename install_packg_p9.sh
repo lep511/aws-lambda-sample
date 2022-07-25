6c8c44501a3bccadcd82bb6100f47d448bc752c4
Esteban Perez <46011885+lep511@users.noreply.github.com>
2022-07-25 14:15:11 -0300

    Create install_packg_p9.sh

---
+++ install_packg_p9.sh
@@ -0,0 +1,30 @@
+read -p "Install python 3.9? (Y/n) " installpy
+read -p "Enter name layer: " namelayer
+read -p "Enter package to install or [req.txt]: " packg
+read -p "Enter region: " region
+
+if [[ "$installpy" =~ ^([yY][eE][sS]|[yY])$ ]]
+    sudo yum update -y
+    sudo amazon-linux-extras install docker -y
+    sudo yum -y groupinstall "Development Tools"
+    sudo yum -y install openssl-devel bzip2-devel libffi-devel
+    sudo yum -y install wget
+    wget https://www.python.org/ftp/python/3.9.10/Python-3.9.10.tgz
+    tar xvf Python-3.9.10.tgz
+    cd Python-*/
+    ./configure --enable-optimizations
+    sudo make altinstall
+    python3.9 -m pip install --upgrade pip
+fi
+
+mkdir -p python
+
+if [[ -z "$packg" ]]; then
+    python3.9 -m pip install -r req.txt -t python/
+else
+    python3.9 -m pip install $packg -t python/
+fi
+
+zip -r layer.zip python
+
+aws lambda publish-layer-version --layer-name $namelayer --zip-file fileb://layer.zip --compatible-runtimes python3.9 --region $region
