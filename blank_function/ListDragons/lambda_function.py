52e3c6fd627ca38dbeba904ae6ffc8c5e6cfa1e2
Esteban Perez <estebanpbuday@outlook.com>
2022-07-26 20:40:43 -0300

    Update

--- blank_function/ListDragons/lambda_function.py
+++ blank_function/ListDragons/lambda_function.py
@@ -3,11 +3,11 @@
 import boto3
 import json
 
-s3 = boto3.client('s3','us-east-1')
 ssm = boto3.client('ssm', 'us-east-1')
 
 def lambda_handler(event, context):
     
+    s3 = boto3.client('s3','us-east-1')
     bucket_name = ssm.get_parameter(
         Name='dragon_data_bucket_name',
         WithDecryption=False)['Parameter']['Value']
@@ -15,28 +15,37 @@ def lambda_handler(event, context):
         Name='dragon_data_file_name',
         WithDecryption=False)['Parameter']['Value']
     
-    expression = "select * from s3object[*][*] s"
+    expression = "SELECT s.* FROM S3Object s"
 
     if 'queryStringParameters' in event and event['queryStringParameters'] is not None:
         if 'dragonName' in event['queryStringParameters']:
-            expression = "select * from S3Object[*][*] s where s.dragon_name_str =  '" + event["queryStringParameters"]['dragonName'] + "'"
+            
+            expression = "SELECT s.* FROM S3Object s WHERE s.dragon_name_str = '" + event["queryStringParameters"]['dragonName'] + "'"
         if 'family' in event['queryStringParameters']:
-            expression = "select * from S3Object[*][*] s where s.family_str =  '" + event["queryStringParameters"]['family'] + "'"
+            expression = "SELECT s.* FROM S3Object s WHERE s.family_str =  '" + event["queryStringParameters"]['family'] + "'"
 
     result = s3.select_object_content(
             Bucket=bucket_name,
-            Key=file_name,
-            ExpressionType='SQL',
+            Key='data_file.json',
             Expression=expression,
-            InputSerialization={'JSON': {'Type': 'Document'}},
-            OutputSerialization={'JSON': {}}
+             ExpressionType='SQL',
+            InputSerialization={
+                'JSON': {
+                    'Type': 'DOCUMENT'
+                }
+            },
+            OutputSerialization={
+                'JSON': {
+                    'RecordDelimiter': ','
+                }
+        }
     )
-    
+
     result_stream = []
     for event in result['Payload']:
         if 'Records' in event:
             for line in event['Records']['Payload'].decode('utf-8').strip().split("\n"):
-                result_stream.append(json.loads(line))
+                result_stream.append(json.loads(json.dumps(line)))
             
         
     return {
@@ -44,5 +53,3 @@ def lambda_handler(event, context):
         "body": json.dumps(result_stream),
         "headers" : {"access-control-allow-origin": "*"}
     }
-
-        
--- blank_function/ValidateDragon/lambda_function.py
+++ blank_function/ValidateDragon/lambda_function.py
@@ -2,6 +2,10 @@
 #####################
 import boto3
 import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 s3 = boto3.client('s3','us-east-1')
 ssm = boto3.client('ssm', 'us-east-1')
@@ -16,29 +20,33 @@ def lambda_handler(event, context):
         Name='dragon_data_file_name',
         WithDecryption=False)['Parameter']['Value']
     
+    logger.info(event['dragon_name_str'])
+    
     result = s3.select_object_content(
         Bucket=bucket_name,
         Key=file_name,
+        Expression="SELECT s.* FROM S3Object s WHERE s.dragon_name_str = '" + event['dragon_name_str'] + "'",
         ExpressionType='SQL',
-        Expression="select * from S3Object[*][*] s where s.dragon_name_str = '" + event['dragon_name_str'] + "'",
-        InputSerialization={'JSON': {'Type': 'Document'}},
-        OutputSerialization={'JSON': {}}
+        InputSerialization={
+            'JSON': {
+                'Type': 'DOCUMENT'
+            }
+        },
+        OutputSerialization={
+            'JSON': {
+                'RecordDelimiter': ','
+            }
+        }
     )
     
     for records in result['Payload']:
         if 'Records' in records:
-            raise DragonValidationException("Duplicate dragon reported")
-        return 'Dragon Validated'
-
-class DragonValidationException(Exception):
-    def __init__(self, *args):
-        if args:
-            self.message = args[0]
-        else:
-            self.message = None
-
-    def __str__(self):
-        if self.message:
-            return 'DragonValidationException, {0}'.format(self.message)
-        else:
-            return 'DragonValidationException has been raised'
+            return {
+                'statusCode': 408,
+                'body': json.dumps("Duplicate dragon reported")
+            }
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps("Dragon Validated")
+    }
