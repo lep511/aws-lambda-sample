3077b666e74a8d15f5575580b97e4493189b1053
Esteban Perez <estebanpbuday@outlook.com>
2022-07-26 14:58:24 -0300

    update

--- blank_function/2-build-layer.sh
+++ blank_function/2-build-layer.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -eo pipefail
 rm -rf package
-cd function
-python3.9 -m pip install -r requirements.txt -t ../package/python
+python3.9 -m pip install -r requirements.txt -t package/python
