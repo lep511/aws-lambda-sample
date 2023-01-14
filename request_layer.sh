0a74956a282efb1233f028746ee657fba417ac4e
Esteban Perez <46011885+lep511@users.noreply.github.com>
2023-01-13 22:20:55 -0300

    Update request_layer.sh

--- request_layer.sh
+++ request_layer.sh
@@ -2,3 +2,4 @@ python3 -m venv test_venv
 source test_venv/bin/activate
 mkdir python  
 pip install requests -t python  
+aws lambda publish-layer-version --layer-name requests --zip-file fileb://requests.zip --compatible-runtimes python3.9 --region us-east-1
