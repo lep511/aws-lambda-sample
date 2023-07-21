0da9d628211d1f5e8df9e5977aa431f45a5d4023
Esteban Pérez <46011885+lep511@users.noreply.github.com>
2023-07-21 10:04:03 -0300

    Update run_docker.sh

--- container/run_docker.sh
+++ container/run_docker.sh
@@ -8,5 +8,7 @@ else
 fi
 
 docker build -t $1 .
-echo Run in another window: curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'
+echo "=================="
+echo "  "
+echo "Run in another window: curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'"
 docker run -p 9000:8080 $1
