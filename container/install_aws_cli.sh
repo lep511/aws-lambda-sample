842b6a6381282098bd46458c96bd084c11737ade
Esteban Pérez <46011885+lep511@users.noreply.github.com>
2023-07-21 10:01:45 -0300

    Update install_aws_cli.sh

--- container/install_aws_cli.sh
+++ container/install_aws_cli.sh
@@ -1,3 +1,6 @@
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+aws configure
+echo "Account ID:"
+aws sts get-caller-identity --query "Account" --output text
