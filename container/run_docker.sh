a370103c3bc58e25cc04d87e58a2296d0e242ba4
Esteban Pérez <46011885+lep511@users.noreply.github.com>
2023-07-21 09:52:43 -0300

    Update run_docker.sh

--- container/run_docker.sh
+++ container/run_docker.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [ -z $1 ]; then
+    echo "DOCKER_IMAGE is not set"
+    echo "Using default docker image name: test-image"
+    export docker_image=test-image
+else
+    export docker_image=$4
+fi
+
 docker build -t $1 .
 echo Run in another window: curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'
 docker run -p 9000:8080 $1
