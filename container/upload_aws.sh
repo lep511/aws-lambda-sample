00a50b241bb1446706b45eb929ba13cb4a08657a
Esteban Pérez <46011885+lep511@users.noreply.github.com>
2023-07-21 11:12:49 -0300

    Update upload_aws.sh

--- container/upload_aws.sh
+++ container/upload_aws.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
-
-#Check aws account id
-if [ -z $1 ]; then
-    echo "ACCOUNT_ID is not set"
-    exit 1
-else
-    export ACCOUNT_ID=$1
-fi
-
-# Check aws region
-if [ -z $2 ]; then
-    echo "AWS_REGION is not set"
-    echo "Using default region: us-east-1"
-    export AWS_REGION=us-east-1
-else
-    export AWS_REGION=$2
-fi
+export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 
 # Check repository name
-if [ -z $3 ]; then
+if [ -z $1 ]; then
     echo "REPOSITORY_NAME is not set"
     echo "Using default repository name: test-repo"
     export repository=test-repo
@@ -27,12 +11,21 @@ else
 fi
 
 # Check docker image name
-if [ -z $4 ]; then
+if [ -z $2 ]; then
     echo "DOCKER_IMAGE is not set"
     echo "Using default docker image name: test-image"
     export docker_image=test-image
 else
     export docker_image=$4
+fi
+
+# Check aws region
+if [ -z $3 ]; then
+    echo "AWS_REGION is not set"
+    echo "Using default region: us-east-1"
+    export AWS_REGION=us-east-1
+else
+    export AWS_REGION=$2
 fi
 
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
